8ae3fc9495b0a7ffcbd4074efe167a8b3cea6af2
Алексей Ковтунец <a.kovtunets@outlook.com>
2018-02-13 16:57:36 +0600

    Signed-off-by: Алексей Ковтунец <a.kovtunets@outlook.com>

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -21,5 +21,10 @@ namespace TestApp
         {
             label1.Text = "Hi";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Buy";
+        }
     }
 }
